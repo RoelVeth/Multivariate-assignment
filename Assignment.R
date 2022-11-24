9803df4ba83e4648fc08f9368d34e151b8021f65
Lenovo-PC\Carlos de Cloet <carlosdecloet@gmail.com>
2022-11-24 14:30:42 +0100

    Hotellings T squared test toegevoegd

--- Assignment.R
+++ Assignment.R
@@ -15,13 +15,18 @@ n = 100
 R = 500
 eps = 0.2
 
+mu_null = c(0,0)
+
 set.seed(2)
 
 results = replicate(R, {
-  y = rmvnorm(n, mean = mu, sigma = Sigma)
+  y = rmvnorm(n, mean = mu, sigma = Sigma) ## bivariate normal model
   contamination = rbinom(n, 1, eps)
-  y = (1-contamination)*y + contamination*cbind(rep(100,n),rep(100,n)) ## Contamination model is here
+  y = (1-contamination)*y + contamination*cbind(rep(50,n),rep(50,n)) ## Contamination model is here
   
+  Test = n*colMeans((y-mu_null))%*%solve(var(y))%*%colMeans((y-mu_null)) ## gebruiken variantie van contaminated model
+  signif = qf(0.95, 2, 98)*2*99/98 # critical value, where p=2, n=100
+  c(Test,signif)
 })
 
 
