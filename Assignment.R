33e16d72372885b516215de047659d0ccd72c557
Lenovo-PC\Carlos de Cloet <carlosdecloet@gmail.com>
2022-11-24 13:00:19 +0100

    r

--- Assignment.R
+++ Assignment.R
@@ -2,3 +2,24 @@
 # Assignment Multivariate Statistics
 # Roel Veth 
 # Carlos de Cloet
+
+
+## Option 2
+
+library('mvtnorm')
+
+rho = 0.0
+mu = c(0,0)
+Sigma = matrix(c(1, rho, rho, 1), nrow = 2, ncol = 2)
+n = 100
+R = 500
+
+set.seed(12030213)
+
+results = replicate(R, {
+  y <- rmvnorm(n, mean = mu, sigma = Sigma)
+})
+
+
+mean(y[,1])
+mean(y[,2])
