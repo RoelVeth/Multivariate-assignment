d659062a8e6e7c666ba7b118aabd3dba3c74db54
RoelVeth <roel.veth@gmail.com>
2022-11-24 13:09:18 +0100

    Kleine test gedaan

--- Assignment.R
+++ Assignment.R
@@ -11,15 +11,14 @@ library('mvtnorm')
 rho = 0.0
 mu = c(0,0)
 Sigma = matrix(c(1, rho, rho, 1), nrow = 2, ncol = 2)
-n = 100
-R = 500
+n = 10
+R = 50
 
-set.seed(12030213)
 
 results = replicate(R, {
-  y <- rmvnorm(n, mean = mu, sigma = Sigma)
+  rmvnorm(n, mean = mu, sigma = Sigma)
+  
 })
 
 
-mean(y[,1])
-mean(y[,2])
+#Hier gaan we nog nuttige dingen typen
