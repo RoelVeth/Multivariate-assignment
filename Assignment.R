30fa1dffa4878072bb0a83122c1067fd497d7277
Lenovo-PC\Carlos de Cloet <carlosdecloet@gmail.com>
2022-11-24 13:55:08 +0100

    contamination toegevoegd

--- Assignment.R
+++ Assignment.R
@@ -11,13 +11,16 @@ library('mvtnorm')
 rho = 0.5
 mu = c(0,0)
 Sigma = matrix(c(1, rho, rho, 1), nrow = 2, ncol = 2)
-n = 10
-R = 50
+n = 100
+R = 500
+eps = 0.2
 
-set.seed(1)
+set.seed(2)
 
 results = replicate(R, {
-  rmvnorm(n, mean = mu, sigma = Sigma)
+  y = rmvnorm(n, mean = mu, sigma = Sigma)
+  contamination = rbinom(n, 1, eps)
+  y = (1-contamination)*y + contamination*cbind(rep(100,n),rep(100,n)) ## Contamination model is here
   
 })
 
