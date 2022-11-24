7c3232b44e0fc7bf12fd5b927ca855a305963e96
Lenovo-PC\Carlos de Cloet <carlosdecloet@gmail.com>
2022-11-24 13:07:46 +0100

    s

--- Assignment.R
+++ Assignment.R
@@ -8,18 +8,15 @@
 
 library('mvtnorm')
 
-rho = 0.0
+rho = 0.5
 mu = c(0,0)
 Sigma = matrix(c(1, rho, rho, 1), nrow = 2, ncol = 2)
 n = 100
 R = 500
 
-set.seed(12030213)
 
 results = replicate(R, {
   y <- rmvnorm(n, mean = mu, sigma = Sigma)
 })
 
 
-mean(y[,1])
-mean(y[,2])
